e88c433e885e154a06046003d9772647a3640ebc
Davi Santos Dias <davisantosdiasm@gmail.com>
2020-12-09 18:10:46 -0300

    final do projeto

--- js/dist/index02.dev.js
+++ js/dist/index02.dev.js
@@ -1,12 +1,10 @@
 "use strict";
 
-//const inpValorMulta = document.querySelector('#idvalorMulta');
-//const spanvalorMulta = document.querySelector('#spanvalorMulta');
 //Valor do boleto
-function inpvalor_Boleto() {
+function valorDoBoleto() {
   var spanvalor_boleto = document.getElementById('spanvalorBoleto');
   var valor_boleto = document.getElementById('idvalorBoleto').value;
-  valor_boleto = Number(valor_boleto).toLocaleString('pt-BR', {
+  valor_boleto = parseFloat(valor_boleto).toLocaleString('pt-BR', {
     style: 'currency',
     currency: 'BRL'
   });
@@ -14,21 +12,21 @@ function inpvalor_Boleto() {
 } //Valor da multa
 
 
-function fu_valorMulta() {
+function valorDaMulta() {
   var spanvalor_multa = document.getElementById('spanvalorMulta');
   var inpvalor_multa = document.getElementById('idvalorMulta').value;
   var valor_boleto = document.getElementById('idvalorBoleto').value;
   valor_boleto = parseFloat(valor_boleto);
   inpvalor_multa = parseInt(inpvalor_multa);
-  var result_valorMulta = parseFloat(valor_boleto * inpvalor_multa / 100).toLocaleString("pt-BR", {
+  var result_valorMulta = Number.parseInt(valor_boleto * inpvalor_multa) / 100;
+  spanvalor_multa.innerHTML = parseFloat(result_valorMulta).toLocaleString('pt-BR', {
     style: 'currency',
     currency: 'BRL'
   });
-  spanvalor_multa.innerHTML = result_valorMulta;
 } //Obtendo dias de atraso.
 
 
-function fun_diasAtraso() {
+function diasDeAtraso() {
   var inputDataVencimento = document.getElementById('idDataVencimento').value;
   var inputDataPagamento = document.getElementById('idDataPagamento').value;
   var spanDiasAtraso = document.getElementById('spanDiasEmAtraso');
@@ -41,31 +39,31 @@ function fun_diasAtraso() {
 } //valor da mora.
 
 
-function fun_valorMora() {
+function valorDaMora() {
   var inputValorMora = document.getElementById('idValorMora').value;
   var spanValorMora = document.getElementById('spanValorDaMora');
-  inputValorMora = Number(inputValorMora);
+  inputValorMora = parseFloat(inputValorMora);
   spanValorMora.innerHTML = inputValorMora;
 } //valor porcentagem do juros.
 
 
-function fun_valorJuros() {
+function valorDoJuros() {
   var inputDataVencimento = document.getElementById('idDataVencimento').value;
   var inputDataPagamento = document.getElementById('idDataPagamento').value;
   var inputValorMora = document.getElementById('idValorMora').value;
   var spanJuros = document.getElementById('spanValorJuros');
-  inputValorMora = Number(inputValorMora);
+  inputValorMora = parseFloat(inputValorMora);
   inputDataVencimento = new Date(inputDataVencimento);
   inputDataPagamento = new Date(inputDataPagamento);
   var umDia = 1000 * 60 * 60 * 24;
-  var diferençaDeTempo = Math.abs(inputDataPagamento - inputDataVencimento);
-  var diferençaDeDias = Math.ceil(diferençaDeTempo / umDia);
-  var result_valorJuros = Number(diferençaDeDias * inputValorMora).toFixed(4);
+  var diferençaDeTempo = Math.abs(parseFloat(inputDataPagamento - inputDataVencimento));
+  var diferençaDeDias = Math.ceil(parseFloat(diferençaDeTempo / umDia));
+  var result_valorJuros = parseFloat(diferençaDeDias * inputValorMora).toFixed(4);
   spanJuros.innerHTML = result_valorJuros;
 } //valor do juros em dinheiro
 
 
-function fun_valorJurosEmdinheiro() {
+function valorDoJurosEmdinheiro() {
   var inputDataVencimento = document.getElementById('idDataVencimento').value;
   var inputDataPagamento = document.getElementById('idDataPagamento').value;
   var valor_boleto = document.getElementById('idvalorBoleto').value;
@@ -77,9 +75,9 @@ function fun_valorJurosEmdinheiro() {
   inputDataPagamento = new Date(inputDataPagamento); //
 
   var umDia = 1000 * 60 * 60 * 24;
-  var diferençaDeTempo = Math.abs(inputDataPagamento - inputDataVencimento);
-  var diferençaDeDias = Math.ceil(diferençaDeTempo / umDia);
-  var valorJuros = parseFloat(diferençaDeDias * inputValorMora);
+  var diferençaDeTempo = Math.abs(parseFloat(inputDataPagamento - inputDataVencimento));
+  var diferençaDeDias = Math.ceil(parseFloat(diferençaDeTempo / umDia));
+  var valorJuros = diferençaDeDias * inputValorMora;
   var resultado_valorJuros = parseFloat(valorJuros * valor_boleto / 100).toLocaleString('pt-BR', {
     style: 'currency',
     currency: 'BRL'
--- js/index02.js
+++ js/index02.js
@@ -1,33 +1,31 @@
-//const inpValorMulta = document.querySelector('#idvalorMulta');
-//const spanvalorMulta = document.querySelector('#spanvalorMulta');
+
 
 //Valor do boleto
-function inpvalor_Boleto() {
+function valorDoBoleto() {
 	let spanvalor_boleto = document.getElementById('spanvalorBoleto');
 	let valor_boleto = document.getElementById('idvalorBoleto').value;
-
-	valor_boleto = Number(valor_boleto).toLocaleString('pt-BR', {
+	valor_boleto = parseFloat(valor_boleto).toLocaleString('pt-BR', {
 		style: 'currency',
 		currency: 'BRL' 
 	}); 
 	spanvalor_boleto.innerHTML = valor_boleto;
 }
 //Valor da multa
-function fu_valorMulta() {
+function valorDaMulta() {
 	let spanvalor_multa = document.getElementById('spanvalorMulta');
 	let inpvalor_multa = document.getElementById('idvalorMulta').value;
 	let valor_boleto = document.getElementById('idvalorBoleto').value;
 
 	valor_boleto = parseFloat(valor_boleto);
 	inpvalor_multa = parseInt(inpvalor_multa);
-	let result_valorMulta = parseFloat((valor_boleto * inpvalor_multa)/100).toLocaleString(`pt-BR`, {
-		style: 'currency',
-		currency: 'BRL'
-	})
-	spanvalor_multa.innerHTML = (result_valorMulta);
+	let result_valorMulta = Number.parseInt(valor_boleto * inpvalor_multa)/100;
+	spanvalor_multa.innerHTML = parseFloat(result_valorMulta).toLocaleString('pt-BR', {
+		style:'currency',
+		currency:'BRL'
+	});
 }
 //Obtendo dias de atraso.
-function fun_diasAtraso() {
+function diasDeAtraso() {
 	let inputDataVencimento = document.getElementById('idDataVencimento').value
 	let inputDataPagamento = document.getElementById('idDataPagamento').value;
 	let spanDiasAtraso = document.getElementById('spanDiasEmAtraso');
@@ -36,50 +34,50 @@ function fun_diasAtraso() {
 	let umDia = 24*60*60*1000;
 	let diferençaDeTempo = Math.abs(inputDataPagamento - inputDataVencimento);
 	let diferençaDeDias = Math.ceil(diferençaDeTempo/umDia);
-	spanDiasAtraso.innerHTML = diferençaDeDias;
+	spanDiasAtraso.innerHTML = (diferençaDeDias);
 
 }
 //valor da mora.
-function fun_valorMora() {
+function valorDaMora() {
 	let inputValorMora = document.getElementById('idValorMora').value;
 	let spanValorMora = document.getElementById('spanValorDaMora')
-	inputValorMora = Number(inputValorMora);
+	inputValorMora = parseFloat(inputValorMora);
 	spanValorMora.innerHTML = inputValorMora;
 }
 //valor porcentagem do juros.
-function fun_valorJuros() {
+function valorDoJuros() {
 	let inputDataVencimento = document.getElementById('idDataVencimento').value
 	let inputDataPagamento = document.getElementById('idDataPagamento').value;
 	let inputValorMora = document.getElementById('idValorMora').value;
 	let spanJuros = document.getElementById('spanValorJuros')
 	
-	inputValorMora = Number(inputValorMora)
+	inputValorMora = parseFloat(inputValorMora)
 	inputDataVencimento = new Date(inputDataVencimento);
 	inputDataPagamento = new Date(inputDataPagamento);
 	
 	let umDia = 1000*60*60*24;
-	let diferençaDeTempo = Math.abs(inputDataPagamento - inputDataVencimento);
-	let diferençaDeDias = Math.ceil(diferençaDeTempo/umDia);
-	let result_valorJuros = Number(diferençaDeDias * inputValorMora).toFixed(4);
+	let diferençaDeTempo = Math.abs(parseFloat(inputDataPagamento - inputDataVencimento));
+	let diferençaDeDias = Math.ceil(parseFloat(diferençaDeTempo/umDia));
+	let result_valorJuros = parseFloat(diferençaDeDias * inputValorMora).toFixed(4);
 	spanJuros.innerHTML = result_valorJuros;
 }
 //valor do juros em dinheiro
-function fun_valorJurosEmdinheiro() {
+function valorDoJurosEmdinheiro() {
 	let inputDataVencimento = document.getElementById('idDataVencimento').value
 	let inputDataPagamento = document.getElementById('idDataPagamento').value;
 	let valor_boleto = document.getElementById('idvalorBoleto').value;
 	let inputValorMora = document.getElementById('idValorMora').value;
-	let spanJurosDinheiro = document.getElementById('spanValorJurosEmDinheiro')
-	valor_boleto = parseFloat(valor_boleto)
-	inputValorMora = parseFloat(inputValorMora)
+	let spanJurosDinheiro = document.getElementById('spanValorJurosEmDinheiro');
+	valor_boleto = parseFloat(valor_boleto);
+	inputValorMora = parseFloat(inputValorMora);
 	inputDataVencimento = new Date(inputDataVencimento);
 	inputDataPagamento = new Date(inputDataPagamento);
 	//
 	let umDia = 1000*60*60*24;
-	let diferençaDeTempo = Math.abs(inputDataPagamento - inputDataVencimento);
-	let diferençaDeDias = Math.ceil(diferençaDeTempo/umDia);
-	let valorJuros = parseFloat(diferençaDeDias * inputValorMora)
-	let resultado_valorJuros = parseFloat((valorJuros) * (valor_boleto)/100).toLocaleString('pt-BR', {
+	let diferençaDeTempo = Math.abs(parseFloat(inputDataPagamento - inputDataVencimento));
+	let diferençaDeDias = Math.ceil(parseFloat(diferençaDeTempo/umDia));
+	let valorJuros = (diferençaDeDias * inputValorMora);
+	let resultado_valorJuros = parseFloat((valorJuros * valor_boleto)/100).toLocaleString('pt-BR', {
 		style:'currency',
 		 currency:'BRL'
 		});
